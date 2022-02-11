1d9ead9403206c1d8b63ce54fed532bb61f8eaf5
Jorge Roldán <icabina@live.com>
2022-02-11 10:11:07 -0500

    m

---
+++ algorithms/HR_oddNumbers.js
@@ -0,0 +1,22 @@
+function oddNumbers(l, r) {
+  if (1 > l || l > r || r > 1000000) return;
+
+  let left = l;
+  let container = [];
+  const isOdd = (num) => num % 2 !== 0;
+
+  do {
+    if (isOdd(left)) {
+      container.push(left);
+    }
+    left = left + 1;
+  } while (left <= r);
+
+  console.log(container);
+  return container;
+}
+//TEST
+console.log("======================== ");
+
+console.log(oddNumbers(7, 15)); //
+console.log(oddNumbers(7, 15)); //
